94c7b92c06519aeab32e60970905fea4b6ac2946
IgorTheOverlord <102486458+IgorTheOverlord@users.noreply.github.com>
2022-09-09 00:06:01 +0300

    Delete app directory

--- app/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByClassName("dog-name")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementsByClassName("edit-button").click();
-    }
-});
